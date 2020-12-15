0b9158edd22e27e61ecbb501574556ae086f765b
sohyeonyun <wja06018@naver.com>
2020-12-15 18:08:46 +0900

    리뷰 디비 연동

--- BARCOMMEND/app/src/main/java/com/example/barcommend/ScrollingActivity.java
+++ BARCOMMEND/app/src/main/java/com/example/barcommend/ScrollingActivity.java
@@ -2,16 +2,12 @@ package com.example.barcommend;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
-import android.widget.RatingBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -19,18 +15,19 @@ import androidx.appcompat.widget.Toolbar;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
-import com.example.barcommend.data.RegisterRequest;
+import com.example.barcommend.data.GetReviewRequest;
 import com.example.barcommend.data.WriteReviewRequest;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
 public class ScrollingActivity extends AppCompatActivity {
     private PopupWindow mPopupWindow;
-    private String scanNum, userID, userName;
+    private String scanNum, userID;
     private String star, strcomment;
 
     @Override
@@ -41,41 +38,86 @@ public class ScrollingActivity extends AppCompatActivity {
         Intent intent = getIntent();
         scanNum = intent.getStringExtra("Gtin");
         userID = intent.getStringExtra("userID");
-        userName = intent.getStringExtra("userName");
 
+        Log.i("ScrollingActivity", "Scrolling 들어옴");
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
         toolBarLayout.setTitle(getTitle());
 
+
+        //리뷰 불러오기
+        //String Gtin = scanNum;
+        final String Gtin = "8801382131038"; //debug!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    Log.i("ScrollingActivity", "리뷰 불러오는중");
+                    JSONArray jsonArray = new JSONArray( response );
+                    Log.i("jsonArray", String.valueOf(jsonArray));
+
+                    //받아온 데이터 하나씩 읽기
+                    String dataArray = null; //Json Array를 String으로 저장하기 위한 변수 선언
+
+                    int list_cnt = jsonArray.length(); //Json 배열 내 JSON 데이터 개수를 가져옴
+                    String[] getUserName = new String[list_cnt]; //userName 저장용
+                    String[] getReviewText = new String[list_cnt];
+                    String[] getStarPoint = new String[list_cnt];
+                    String[] getReviewDate = new String[list_cnt];
+
+                    for (int i = 0; i < list_cnt; i++) { //JSONArray 내 json 개수만큼 for문 동작
+                        JSONObject jsonObject = jsonArray.getJSONObject(i); //i번째 Json데이터를 가져옴
+                        getUserName[i] = jsonObject.getString("userName");  //userName 값을 배열에 저장
+                        getReviewText[i] = jsonObject.getString("ReviewText");
+                        getStarPoint[i] = jsonObject.getString("StarPoint");
+                        getReviewDate[i] = jsonObject.getString("ReviewDate");
+                        Log.i("JSON Object", jsonObject + "");
+                        Log.i("JsonParsing", getUserName[i] + "," + getReviewText[i] + "," + getStarPoint[i] + "," + getReviewDate[i]);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        GetReviewRequest getreviewRequest = new GetReviewRequest( Gtin, responseListener );
+        RequestQueue queue = Volley.newRequestQueue( ScrollingActivity.this );
+        queue.add( getreviewRequest );
+
+        //리뷰 화면에 보여주기
+        //todo
+
+
+        //리뷰 추가 버튼 누를 시
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.i("ScrollingActivity", "리뷰 추가 중");
                 View popupView = getLayoutInflater().inflate(R.layout.activity_comment, null);
                 mPopupWindow = new PopupWindow(popupView, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
                 mPopupWindow.setFocusable(true);
                 mPopupWindow.showAtLocation(popupView, Gravity.CENTER, 0, 0);
 
-                star = "3"; //for debugggggggggggg
-                strcomment = "GOODGOOD";
-
-
+                //리뷰 저장 버튼 누르면
                 Button cancel = (Button) popupView.findViewById(R.id.btn_confirm);
                 cancel.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Toast.makeText(getApplicationContext(), "Completed " + star + strcomment, Toast.LENGTH_LONG).show();
-                        mPopupWindow.dismiss();
+                        //xml에서 별점, 코멘트 받아오기
+                        star = "4.5"; //for debugggggggggggg
+                        strcomment = "HAHA";
 
+                        //
+                        mPopupWindow.dismiss();
 
                         //입력받은 별점, 후기와 작성일, 작성자 디비에 넣기
                         //userID(String), Gtin(String), ReviewText(String), StarPoint(Double), ReviewDate(String)
                         //String userID, scanNum
-                        String ReviewText = strcomment;
-                        Double StarPoint = Double.valueOf(star);
-                        String ReviewDate = "2020-12-14";
-                        Toast.makeText( getApplicationContext(), "성공", Toast.LENGTH_SHORT ).show();
+                        final String ReviewText = strcomment;
+                        final Double StarPoint = Double.valueOf(star);
+                        final String ReviewDate = "2020-12-14";
 
                         Response.Listener<String> responseListener = new Response.Listener<String>() {
                             @Override
@@ -84,29 +126,21 @@ public class ScrollingActivity extends AppCompatActivity {
                                 try {
                                     JSONObject jsonObject = new JSONObject( response );
                                     boolean success = jsonObject.getBoolean( "success" );
-
-                                    //회원가입 성공시
+                                    //후기 추가 성공시
                                     if(success) {
-
-                                        Toast.makeText( getApplicationContext(), "성공", Toast.LENGTH_SHORT ).show();
-                                        //Intent intent = new Intent( RegisterActivity.this, LoginActivity.class );
-                                        //startActivity( intent );
-
-                                        //회원가입 실패시
+                                        Log.i("리뷰 추가 성공", ReviewText + ", " + StarPoint + ", " + ReviewDate);
+                                        //후기 추가 실패시
                                     } else {
-                                        Toast.makeText( getApplicationContext(), "실패", Toast.LENGTH_SHORT ).show();
+                                        //Toast.makeText( getApplicationContext(), "실패", Toast.LENGTH_SHORT ).show();
                                         return;
                                     }
-
                                 } catch (JSONException e) {
                                     e.printStackTrace();
                                 }
-
                             }
                         };
-
                         //서버로 Volley를 이용해서 요청
-                        WriteReviewRequest writereviewRequest = new WriteReviewRequest( userID, scanNum, ReviewText, StarPoint, ReviewDate, responseListener);
+                        WriteReviewRequest writereviewRequest = new WriteReviewRequest( userID, Gtin, ReviewText, StarPoint, ReviewDate, responseListener);
                         RequestQueue queue = Volley.newRequestQueue( ScrollingActivity.this );
                         queue.add( writereviewRequest );
 
@@ -114,8 +148,6 @@ public class ScrollingActivity extends AppCompatActivity {
                 });
             }
         });
-
-
 
     }
 }
